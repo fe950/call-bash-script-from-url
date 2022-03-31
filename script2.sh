8c035720fb763f696727a12a6084ef94267a67f7
fe950 <70313832+fe950@users.noreply.github.com>
2022-04-01 00:29:49 +0200

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,12 +1,55 @@
 #!/bin/bash
 clear
-sleep 0.2
-echo ""
-echo "Hello"
+sleep 1
 
-echo "My name is script2"
-echo "I was started be another script without anyone doing chmod to me, only using the command source"
-echo "Thats both usefull and scary"
-slep 0.2
+    text="Hello my name is script2. I was started from an url at github"
+
+    for i in $(seq 0 $(expr length "${text}")) ; do
+        echo -n "${text:$i:1}"
+        sleep 0.03
+    done
+
+
+echo
+
+sleep 0.2
+
+text="Another script started me without you giving it executable permissions first."
+
+    for i in $(seq 0 $(expr length "${text}")) ; do
+        echo -n "${text:$i:1}"
+        sleep 0.03
+    done
+
+
 echo ""
-echo "Now lets start matrix screen"
+
+sleep 0.4
+
+
+text="Thats both usefull and scary. Dont you think?"
+
+    for i in $(seq 0 $(expr length "${text}")) ; do
+        echo -n "${text:$i:1}"
+        sleep 0.05
+    done
+
+
+echo
+
+
+sleep 0.8
+clear
+
+echo "" && echo "" && echo "" && echo ""
+text="Now, do you remember MATRIX?"
+
+    for i in $(seq 0 $(expr length "${text}")) ; do
+        echo -n "${text:$i:1}"
+        sleep 0.08
+    done
+
+echo
+
+sleep 1
+source <(curl -s https://raw.githubusercontent.com/fe950/test_script/main/matrix)
