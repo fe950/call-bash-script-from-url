1b6570a006c7477b067819aaea20b8c0b83e0670
fe950 <70313832+fe950@users.noreply.github.com>
2022-04-01 00:18:26 +0200

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+clear
+sleep 0.2
 echo ""
 echo "Hello"
+
 echo "My name is script2"
+echo "I was started be another script without anyone doing chmod to me, only using the command source"
+echo "Thats both usefull and scary"
+slep 0.2
 echo ""
+echo "Now lets start matrix screen"
